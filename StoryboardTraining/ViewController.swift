036fd8110766f40d259cbac28b8c0fbb7bb0cd9c
oTranThanhNghia <thanhnghiaglhn92@gmail.com>
2020-02-20 16:03:09 +0700

    Initial Commit

---
+++ StoryboardTraining/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoryboardTraining
+//
+//  Created by Nghia Tran on 2/20/20.
+//  Copyright © 2020 Nghia Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
