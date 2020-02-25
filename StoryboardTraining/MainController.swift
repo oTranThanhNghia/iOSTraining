8c5e4c49ae2298be564c05170d1992b1fdfadd83
oTranThanhNghia <thanhnghiaglhn92@gmail.com>
2020-02-25 13:53:09 +0700

    add a square

--- StoryboardTraining/MainController.swift
+++ StoryboardTraining/MainController.swift
@@ -10,14 +10,40 @@ import UIKit
 
 class MainController:UIViewController{
     @IBOutlet weak var TxtUserName: UILabel!
-    
+    let squareButton = UIView()
     var userName=""
+    
+    var minRange: CGFloat = 0.0
+    
     override func loadView() {
         super.loadView()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let height = view.frame.size.height
+        let width = view.frame.size.width
+        NSLog("MainController viewDidLoad view height: \(height) width: \(width)")
+        minRange = min(height, width)
+        NSLog("MainController viewDidLoad view minRange: \(minRange)")
         TxtUserName.text = "Hello user: " + userName
+        setupSquareButton()
+        
     }
+    
+    func setupSquareButton() {
+        squareButton.backgroundColor = .red
+            
+        view.addSubview(squareButton)
+            
+        squareButton.translatesAutoresizingMaskIntoConstraints = false
+            
+        squareButton.heightAnchor.constraint(equalToConstant: minRange/2).isActive = true
+        squareButton.widthAnchor.constraint(equalToConstant: minRange/2).isActive = true
+        squareButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        squareButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+            
+    }
+    
+    
 }
